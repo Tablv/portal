3c9d958c73c39efe64243a6257de6cab34c059ec
lvchangjie <1204479385@qq.comm>
2020-07-31 17:59:42 +0800

    加入临时子导航

--- src/components/nav/menu.vue
+++ src/components/nav/menu.vue
@@ -5,8 +5,18 @@
       @click="doClick(item)"
       v-for="item in menuList"
       :key="item.key"
+      :active="item.key === activeKey"
     >
       {{item.name}}
+      <template>
+        <div class="sub-mune" v-show="item.subMune">
+          <ul class="sub-mune-ul">
+            <li class="sub-mune-ul-li">仪表盘</li>
+            <li class="sub-mune-ul-li">故事板</li>
+            <li class="sub-mune-ul-li">hello</li>
+          </ul>
+        </div>
+      </template>
     </li>
   </ul>
 </template>
@@ -18,14 +28,19 @@ export default {
   data() {
     return {
       menuList: [
-        { key:'index', name: '首页' },
-        { key:'product', name: '产品服务' },
-        { key:'404', name: '解决方案' },
-        { key:'404', name: '云市场' },
-        { key:'404', name: '公司动态' },
-        { key:'404', name: '招贤纳士' },
-        { key:'404', name: '关于我们' }
+        { key:'index', name: '首页', subMune: false },
+        { key:'product', name: '产品服务', subMune: true },
+        { key:'4041', name: '解决方案', subMune: true },
+        { key:'4042', name: '云市场', subMune: true },
+        { key:'4043', name: '公司动态', subMune: true },
+        { key:'4045', name: '招贤纳士', subMune: false },
+        { key:'4046', name: '关于我们', subMune: false }
       ]
+    }
+  },
+  computed: {
+    activeKey() {
+      return this.$route.name;
     }
   },
   methods: {
@@ -35,3 +50,30 @@ export default {
   }
 }
 </script>
+
+<style lang="less">
+  .menu-bar-item:hover > .sub-mune {
+    display: block;
+  }
+  .sub-mune {
+    background: #07144e;
+    display: none;
+    border-radius: 0 0 5px 5px;
+    box-shadow: 8px 8px 14px -18px #a7bafd;
+    padding-bottom: 16px;
+    .sub-mune-ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      color: #fff;
+      &-li {
+        font-size: 14px;
+        margin: 0;
+        line-height: 40px;
+        &:hover {
+          font-size: 16px;
+        }
+      }
+    }
+  }
+</style>
